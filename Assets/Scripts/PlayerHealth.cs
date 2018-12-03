8e07450b0a58989ace692274f6bf6138ccd31ba6
Jonas Almström <jonas.almstrom@elev.ga.lbs.se>
2018-12-03 08:19:48 +0100

    Comments

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI;// gör så att vi kan använda Unity UI
 using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
@@ -12,33 +12,38 @@ public class PlayerHealth : MonoBehaviour
     public Slider healthbar;
 
     public float currentHealth;
-    
+
 
 
 
     private void Start()
     {
+        //sätter currentHealth till health vilket är maxhealth 
         currentHealth = health;
-
+        //sätter valuet på healthbaren till calculatehealth
         healthbar.value = CalculateHealth();
     }
 
+    //gör en funktion där man kan sätta in en float inom parantesen beroende på situationen
     void DealDamage(float damageValue)
     {
         currentHealth -= damageValue;
+        //sätter healthbaren till calculatehealth igen
         healthbar.value = CalculateHealth();
-        
-    } 
+
+    }
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //om objektet kolliderar med något som har taggen SmallEnemy så kör den funktionen DealDamage med 50 som värde
         if (collision.gameObject.tag == "SmallEnemy")
         {
             DealDamage(50f);
         }
     }
 
+    //gör en float vid namn calculatehealth som returnerar värdet av currentHealth / health
     float CalculateHealth()
     {
         return currentHealth / health;
@@ -46,7 +51,7 @@ public class PlayerHealth : MonoBehaviour
 
     void Update()
     {
-
+        //om currenthealth är mindre än eller 0 så restartar den scenen
         if (currentHealth <= 0f)
         {
             Scene active = SceneManager.GetActiveScene();
